4dad59c980f9ff3173112f20718580a83e26fe11
Xavi1594 <xavi9415@hotmail.com>
2023-02-15 09:25:54 +0100

    establecida conexion tablas

--- src/main/java/com/smile/smile/model/PatientModel.java
+++ src/main/java/com/smile/smile/model/PatientModel.java
@@ -1,5 +1,24 @@
 package com.smile.smile.model;
 
+import javax.persistence.Column;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "patients")
 public class PatientModel {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id__patients")
+    public Long id;
+
+    public PatientModel(Long id) {
+        this.id = id;
+    }
+
 }
