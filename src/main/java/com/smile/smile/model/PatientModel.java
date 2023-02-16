0bc33e3118fbe7b746e1aea32ffca843cc5e5b42
Caren Salles <carencrisbsalles@gmail.com>
2023-02-16 10:05:15 +0100

    updated

--- src/main/java/com/smile/smile/model/PatientModel.java
+++ src/main/java/com/smile/smile/model/PatientModel.java
@@ -1,14 +1,7 @@
 package com.smile.smile.model;
 
 import javax.persistence.Column;
-<<<<<<< HEAD
 import javax.persistence.Entity;
-=======
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
->>>>>>> 4dad59c980f9ff3173112f20718580a83e26fe11
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -17,7 +10,6 @@ import javax.persistence.Table;
 public class PatientModel {
 
     @Id
-<<<<<<< HEAD
     @Column(name = "dni")
 
     private String dniPatient;
@@ -28,14 +20,6 @@ public class PatientModel {
 
     public void setDniPatient(String dniPatient) {
         this.dniPatient = dniPatient;
-=======
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id__patients")
-    public Long id;
-
-    public PatientModel(Long id) {
-        this.id = id;
->>>>>>> 4dad59c980f9ff3173112f20718580a83e26fe11
     }
 
 }
